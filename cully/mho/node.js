66e10b2803f957d471e2582aea69a531f93fec4d
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-09-07 21:30:35 +0800

    Update node.js

--- cully/mho/node.js
+++ cully/mho/node.js
@@ -1,1 +1,14 @@
-
+    {
+    "categories": [
+            {
+            "name": "National Tv",
+            "channels": [
+               {
+                    "drm_id": "moji",
+                    "referer": "https:\/\/www.visionplus.id",
+                    "logo_url": "https:\/\/s.id\/29CyY",
+                    "name": "MOJI",
+                    "stream_url": "https:\/\/atemecdnbalancer-voe.sysln.id\/live\/eds\/MOJIOChannel\/mpd\/MOJIOChannel.mpd",
+                    "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
+                },
+               
