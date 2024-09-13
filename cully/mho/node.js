c00124b58e36468c8e6e866bee6c0e0d76178387
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-09-14 02:39:11 +0800

    Update node.js

--- cully/mho/node.js
+++ cully/mho/node.js
@@ -4,11 +4,11 @@
             "name": "National Tv",
             "channels": [
             {       
-                    "referer": "https:\/\/keralamaxtv.com\/sliv\/play.php?id=785",
+                    "referer": "https://mhdtv.co.in/daddy/?play=91",
                     "logo_url": "https:\/\/blogger.googleusercontent.com\/img\/b\/R29vZ2xl\/AVvXsEhlbvYw4ae07Ir0IMfk0BGj1WrA6DDfO7sS28Q3pIMCo7pmS6mLisK8AbhrDJg1breEticAzwEx3z_8ATkRYlDrWFfN8yWMsWFWeHIjLO44P-n80rDP3duG0fClfiIbBIsyLTwWBFuMddwi_-Y9XlcagzJRW3WGXB35cbzB8t5SEsu-NazI1XAbagrWNVw\/s1280\/20240910_003215.jpg",
                     "name": "Tv 1",
-                    "stream_url": "https:\/\/keralamaxtv.com\/sliv\/stream.php?id=785",
-                    "user_agent": "Mozilla/5.0 (Web0S; Linux/SmartTV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36 DMOST/2.0.0 (; LGE; webOSTV; WEBOS6.3.2 03.34.95; W6_lm21a;) ExoPlayerLib/2.13.2"
+                    "stream_url": "https://mhdtv.co.in/daddy/stream.m3u8?id=91",
+                    "user_agent": "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5"
                 },       
                {
                     "logo_url": "https:\/\/blogger.googleusercontent.com\/img\/b\/R29vZ2xl\/AVvXsEhlbvYw4ae07Ir0IMfk0BGj1WrA6DDfO7sS28Q3pIMCo7pmS6mLisK8AbhrDJg1breEticAzwEx3z_8ATkRYlDrWFfN8yWMsWFWeHIjLO44P-n80rDP3duG0fClfiIbBIsyLTwWBFuMddwi_-Y9XlcagzJRW3WGXB35cbzB8t5SEsu-NazI1XAbagrWNVw\/s1280\/20240910_003215.jpg",
