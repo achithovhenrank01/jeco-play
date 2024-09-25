d3d2ad19453b144af7259a97e11ba2d97bf0d503
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-09-26 02:18:54 +0800

    Update node.js

--- cully/mho/node.js
+++ cully/mho/node.js
@@ -1,4 +1,2 @@
 #EXTINF:-1 tvg-id="sport1plus.de" tvg-name="DE: SPORT1+ FHD" tvg-logo="https://i.pinimg.com/236x/56/ca/43/56ca434aa49fc1f11c7a7558561bc677.jpg" group-title="馃敶PREMIUM SPORT", SPORT1+ 
-#KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=c1c11c3844b0dffdb9d9831900f1a1da:a2c31e15346f339ca2b47bdd8591553f
-https://p7s1-live-009-prod.akamaized.net/c5c609cf/t_009/sport1plus-de-hd/cenc-default.mpd
+https://achithovhenrank01.github.io/jeco-play/Gratis.mp4
