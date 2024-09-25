81a6dfb1107caaa3002a343ba364dc4a7acf795b
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-09-26 01:41:03 +0800

    Update node.js

--- cully/mho/node.js
+++ cully/mho/node.js
@@ -4,9 +4,21 @@
             "name": "National Tv",
             "channels": [
             {
-                    "logo_url": "https:\/\/s.id\/tekB7",
+                    "logo_url": "https:\/\/s.id\/5UP0q",
                     "name": "A : Peringatan",
                     "stream_url": "https:\/\/achithovhenrank01.github.io\/jeco-play\/Gratis.mp4",
+                    "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
+                },
+                {
+                    "logo_url": "https:\/\/s.id\/EnXxl",
+                    "name": "A : Tips Donasi",
+                    "stream_url": "https:\/\/achithovhenrank01.github.io\/jeco-play\/Photo\/Tips%20donasi.mp4",
+                    "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
+                },
+                {
+                    "logo_url": "https:\/\/s.id\/SrEVD",
+                    "name": "A : Donasi",
+                    "stream_url": "https:\/\/achithovhenrank01.github.io\/jeco-play\/Photo\/Donasi.mp4",
                     "user_agent": "Mozilla\/5.0 (Linux; Android 9; CPH2083 Build\/PPR1.180610.011) AppleWebKit\/537.36"
                 },
                {
