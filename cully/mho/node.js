fb7bd4bd36d93237af612d7acd9807f77e234c77
achithovhenrank01 <37116284+achithovhenrank01@users.noreply.github.com>
2024-09-26 02:24:34 +0800

    Update node.js

--- cully/mho/node.js
+++ cully/mho/node.js
@@ -1,2 +1,2 @@
-#EXTINF:-1 tvg-id="sport1plus.de" tvg-name="DE: SPORT1+ FHD" tvg-logo="https://s.id/5UP0q" group-title="PERIGATAN", SPORT1+ 
+#EXTINF:-1 tvg-id="sport1plus.de" tvg-name="DE: SPORT1+ FHD" tvg-logo="https://s.id/5UP0q" group-title="PERIGATAN", warning
 https://achithovhenrank01.github.io/jeco-play/Gratis.mp4
